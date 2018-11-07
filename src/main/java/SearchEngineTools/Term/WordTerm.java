e4e54a50ebcf230b32eefc598bd71e96cb3044cf
benarili <benarili@post.bgu.ac.il>
2018-11-07 14:15:28 +0200

    deleted ITerm, all implementation is in ATerm changed parse to return collection

--- src/main/java/SearchEngineTools/Term/WordTerm.java
+++ src/main/java/SearchEngineTools/Term/WordTerm.java
@@ -11,4 +11,42 @@ public class WordTerm extends ATerm {
     public String getTerm() {
         return term;
     }
+
+    public void setTerm(String term){
+        this.term=term;
+        removePunctuation();
+    }
+
+    public void toLowerCase(){
+        this.term = this.term.toLowerCase();
+    }
+
+    public void toUperCase(){
+        this.term = this.term.toUpperCase();
+    }
+
+
+    private void removePunctuation(){
+        int beginIndex = 0;
+        int endIndex = term.length()-1;
+        boolean isRelevantBeginning = false;
+        boolean isRelevantEnd = false;
+
+        //find relevant beginning and end indexes
+        while (beginIndex< term.length() && !isRelevantBeginning){
+            if(Character.isDigit(term.charAt(beginIndex)) || Character.isLetter(term.charAt(beginIndex)))
+                isRelevantBeginning = true;
+            else
+                beginIndex++;
+        }
+
+        while (endIndex >= beginIndex && !isRelevantEnd){
+            if(Character.isDigit(term.charAt(beginIndex)) || Character.isLetter(term.charAt(beginIndex)))
+                isRelevantEnd = true;
+            else
+                endIndex--;
+        }
+
+        term = term.substring(beginIndex,endIndex+1);
+    }
 }
