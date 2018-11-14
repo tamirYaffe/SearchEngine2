a8225fa560792b9850067d3b8a4c69b1f9ddcb0f
tamiryaffe <tamiryaffe144@gmail.com>
2018-11-14 16:05:19 +0200

    test change

--- src/main/java/SearchEngineTools/PostingList.java
+++ src/main/java/SearchEngineTools/PostingList.java
@@ -1,14 +1,13 @@
 package SearchEngineTools;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class PostingList {
-    private List<PostingEntry> postingList;
+    private Map<Integer,Integer> postingListMap;
     private String term;
 
     public PostingList(String term) {
-        this.postingList = new ArrayList<>();
+        this.postingListMap = new LinkedHashMap<>();
         this.term = term;
     }
 
@@ -16,15 +15,35 @@ public class PostingList {
         return term;
     }
 
-    public PostingEntry get(int index){
-        return postingList.get(index);
-    }
-
-    public List<PostingEntry> toPostingList() {
+    public List<PostingEntry> toList() {
+        List<PostingEntry> postingList= new ArrayList<>();
+        for(int docId:postingListMap.keySet()){
+            postingList.add(new PostingEntry(docId,postingListMap.get(docId)));
+        }
         return postingList;
     }
 
-    public void add(PostingEntry postingEntry) {
-        postingList.add(postingEntry);
+    /**
+     *
+     * @param postingEntry
+     * @return added size in bytes
+     */
+    public int add(PostingEntry postingEntry) {
+        if(!postingListMap.containsKey(postingEntry.getDocID())){
+            postingListMap.put(postingEntry.getDocID(),postingEntry.getTermTF());
+            return postingEntry.getSizeInBytes();
+        }
+        else{
+            String oldSize=""+postingListMap.get(postingEntry.getDocID());
+            addToOccurrences(postingEntry.getDocID(),postingEntry.getTermTF());
+            String newSize=""+postingListMap.get(postingEntry.getDocID());
+            int addedSize=newSize.length()-oldSize.length();
+            return addedSize;
+        }
+    }
+
+
+    private void addToOccurrences(int docID, int occurrencesToAdd) {
+        postingListMap.replace(docID,postingListMap.get(docID)+occurrencesToAdd);
     }
 }
