8898ac8e3eb93b8d319c7a475031bb02b09008e6
tamiryaffe <tamiryaffe144@gmail.com>
2018-11-17 22:10:18 +0200

    test change

--- src/main/java/SearchEngineTools/PostingListComparator.java
+++ src/main/java/SearchEngineTools/PostingListComparator.java
@@ -7,8 +7,13 @@ import java.util.Comparator;
 public class PostingListComparator implements Comparator<Pair<String, Integer>> {
     @Override
     public int compare(Pair<String, Integer> o1, Pair<String, Integer> o2) {
-        if(o1.getKey().split(" ")[0].equals(o2.getKey().split(" ")[0]))
-            return -1;
-        return 0;
+        int compareResult=o1.getKey().split(" ")[0].compareTo(o2.getKey().split(" ")[0]);
+        if(compareResult==0){
+            int  docID1= Integer.parseInt((o1.getKey().split(" ")[o1.getKey().split(" ").length-2]));
+            int docID2= Integer.parseInt((o2.getKey().split(" ")[o2.getKey().split(" ").length-2]));
+            return docID1 - docID2;
+        }
+         else
+             return compareResult;
     }
 }
